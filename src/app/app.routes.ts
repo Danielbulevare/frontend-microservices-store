552ca632125228e86cf253a901e912445d63d7a6
Dan <daniel_vazrobk@hotmail.com>
2025-02-24 18:28:32 -0600

    Se establecio el routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'store',
+        loadComponent: () => import('./Shared/components/header/header.component')
+    },
+    {
+        path:'',
+        redirectTo:'/store',
+        pathMatch: 'full'
+    }
+];
