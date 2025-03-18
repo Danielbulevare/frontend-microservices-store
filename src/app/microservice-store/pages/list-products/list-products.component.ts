7dcb2ca7032b321c3ffe02b1935df2fba37aa249
Dan <daniel_vazrobk@hotmail.com>
2025-03-17 19:31:19 -0600

    Se creo el componente de paginacion y se agrego al componente de list-products

--- src/app/microservice-store/pages/list-products/list-products.component.ts
+++ src/app/microservice-store/pages/list-products/list-products.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { PaginationComponent } from '../../../Shared/components/pagination/pagination.component';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-list-products',
-  imports: [],
+  imports: [PaginationComponent],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css',
 })
 export default class ListProductsComponent {
   constructor() {}
-  
 }
