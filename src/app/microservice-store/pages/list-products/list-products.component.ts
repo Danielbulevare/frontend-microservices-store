022616a78d5e7dbaedf7d9077a00c8233947496a
Dan <daniel_vazrobk@hotmail.com>
2025-03-17 19:22:27 -0600

    Se maqueto el diseño del componente list-products

--- src/app/microservice-store/pages/list-products/list-products.component.ts
+++ src/app/microservice-store/pages/list-products/list-products.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-list-products',
   imports: [],
   templateUrl: './list-products.component.html',
-  styleUrl: './list-products.component.css'
+  styleUrl: './list-products.component.css',
 })
 export default class ListProductsComponent {
-
+  constructor() {}
+  
 }
